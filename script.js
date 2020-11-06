d2201e4a76f2142a1e6a5e48b7c9bf99c186d74a
example <alisayar0107@gmail.com>
2020-11-06 10:38:16 +0300

    loading spinner added.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-// Declarin
-const quoteContainer = document.getElementById('quote-container')
-const quoteText = document.getElementById('quote')
-const authorText = document.getElementById('author')
-const twitterBtn = document.getElementById('twitter')
-const newQuoteBtn = document.getElementById('new-quote')
+// Declaring variables from index.html
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+ function loading() {
+   loader.hidden = false;
+   quoteContainer.hidden = true;
+ }
+
+ // Hide Loading
+ function complete() {
+   if (!loader.hidden) {
+     quoteContainer.hidden = false;
+     loader.hidden = true;
+   }
+ }
 
 // Get Quote From API
 async function getQuote () {
+loading();
 const proxyUrl = 'https://salty-harbor-31993.herokuapp.com/'
 const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
@@ -30,6 +45,9 @@ const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&forma
   }
 
    quoteText.innerText = data.quoteText;
+
+   // Stop Loader and show the quote.
+   complete();
 
  } catch (error) {
      getQuote(); 
