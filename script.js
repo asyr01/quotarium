7cdbdb4a340082cc977417c1165377796e94b08e
example <alisayar0107@gmail.com>
2020-11-10 13:03:46 +0300

    some major improvements on js, grid changed to the flex on nav.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const favBtn = document.getElementById('fav');
-const favQuoteNav = document.getElementById('favquote');
-const favAuthNav = document.getElementById('favauthor');
+const main = document.getElementById('fav-quotes');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+
+
+let data = []; // we'll putt all data to here
 
  function showLoadingSpinner() {
    loader.hidden = false;
@@ -29,30 +31,34 @@ showLoadingSpinner();
 const proxyUrl = 'https://salty-harbor-31993.herokuapp.com/'
 const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
-
  try {
    const response = await fetch(proxyUrl + apiUrl);
    const data = await response.json();
+   const newQuote = {
+     quote: `${data.quoteText}`,
+     author: `${data.quoteAuthor}`, 
+   };
 
- 
+   addData(newQuote);
+   
 
    // If Quote Author field is empty, so print Unknown.
-   if(data.quoteAuthor === '') {
+   if(newQuote.author === '') {
      authorText.innerText = 'Unknown'
    } else {
-     authorText.innerText = data.quoteAuthor;
+     authorText.innerText = newQuote.author;
    }
 
   // Reduce font size for longer quotes than 120 characters.
 
-  if(data.quoteText.length > 120) {
+  if(newQuote.quote > 120) {
     quoteText.classList.add('long-quote')
   } else {
     quoteText.classList.remove('long-quote')
   }
    
    favBtn.classList.remove('clicked');
-   quoteText.innerText = data.quoteText;
+   quoteText.innerText = newQuote.quote;
 
    removeLoadingSpinner();
       
@@ -62,6 +68,27 @@ const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&forma
  }
 
 }
+
+// For adding new quote to the data array.
+function addData(obj){
+  data.push(obj);
+}
+
+// update DOM --forEach--
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Quote</strong>Author</h2>';
+  //take providedData
+  if(favBtn.classList.contains('clicked')){
+    providedData.forEach((item) => {
+      const element = document.createElement('div');
+      element.classList.add('fav__quote');
+      element.innerHTML = `<strong>${item.quote}</strong> <i>${item.author}<i>`;
+      main.appendChild(element);
+     });
+  }
+}
+
 
 // For Tweeting Quotes
 function tweetQuote() {
@@ -82,21 +109,13 @@ function favQuote() {
     if(key && value){
        // and button is clicked
       if(favBtn.classList.contains('clicked')){
-          localStorage.setItem(key,value);
-
-          for(let i = 0; i < localStorage.length; i++){
-            const key = localStorage.key(i);
-            const value = localStorage.getItem(key);
-            favQuoteNav.innerHTML += `${key} : ${value}<br /> <br />`;
-            
+        localStorage.setItem(key, value);
+        updateDOM();
+        } else {
+          localStorage.removeItem(key);
         }
-      } else {
-        localStorage.removeItem(key);
-        favQuoteNav.innerText = '';
-        favAuthNav.innerText = '';
       }
-    }
-}
+     } 
 
 
 // Event Listeners
