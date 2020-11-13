b16d251df83373ca2a975e26ff7a37fb72001506
example <alisayar0107@gmail.com>
2020-11-13 13:10:55 +0300

    script is completed, the app using localstorage, everything works correctly, project is almost completed.

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ const main = document.getElementById('fav-quotes');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-
-let data = []; // we'll putt all data to here
+updateDOM() 
 
  function showLoadingSpinner() {
    loader.hidden = false;
@@ -23,8 +22,6 @@ let data = []; // we'll putt all data to here
    }
  }
 
-
-
 // Get Quote From API
 async function getQuote () {
 showLoadingSpinner();
@@ -34,61 +31,44 @@ const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&forma
  try {
    const response = await fetch(proxyUrl + apiUrl);
    const data = await response.json();
-   const newQuote = {
-     quote: data.quoteText,
-     author: data.quoteAuthor,
-   };
-    
-   addData(newQuote);
-
+ 
    // If Quote Author field is empty, so print Unknown.
-   if(newQuote.author === '') {
+   if(data.authorText === '') {
      authorText.innerText = 'Unknown'
    } else {
-     authorText.innerText = newQuote.author;
+     authorText.innerText = data.quoteAuthor;
    }
 
   // Reduce font size for longer quotes than 120 characters.
 
-  if(newQuote.quote > 120) {
+  if(data.quoteText > 120) {
     quoteText.classList.add('long-quote')
   } else {
     quoteText.classList.remove('long-quote')
   }
-    
+  
    favBtn.classList.remove('clicked');  
-
-   quoteText.innerText = newQuote.quote;
- 
+   quoteText.innerText = data.quoteText;
    removeLoadingSpinner();
-      
 
  } catch (error) {
     getQuote(); 
  }
-
 }
 
-// For adding new quote to the data array.
-function addData(obj){
-  if(favBtn.classList.contains('clicked')){
-    data.push(obj);
-  } else {
-    console.log('error');
-  }
-}
 
-// update DOM --forEach--
-function updateDOM(providedData = data ) {
+
+// update hamburger's DOM 
+function updateDOM() {
   // Clear main div
   main.innerHTML = `<h2><strong>Quote</strong>Author</h2>`;
   //take providedData
-    providedData.forEach((item) => {
-        const element = document.createElement('div');
-        element.classList.add('fav__quote');
-        element.innerHTML = `<strong>${item.quote}</strong> <i>${item.author}<i>`;
-        main.appendChild(element); 
-     });
+    for(let i = 0; i < localStorage.length; i++) {
+      const element = document.createElement('div');
+      element.classList.add('fav__quote');
+      element.innerHTML = `<strong>${localStorage.key(i)}</strong> <i class="cool-text">${localStorage.getItem(localStorage.key(i))}<i>`;
+      main.appendChild(element); 
+    }
   }
 
 // For Tweeting Quotes
@@ -103,8 +83,8 @@ function tweetQuote() {
 // Mark a quote as your favourite quote.
 function favQuote() {
   favBtn.classList.toggle('clicked');
-  key = quoteText.innerText;
-  value = authorText.innerText;
+  let key = quoteText.innerText;
+  let value = authorText.innerText;
  // if we have both key and value
     if(key && value){
        // and button is clicked
@@ -113,6 +93,7 @@ function favQuote() {
         updateDOM();
         } else {
           localStorage.removeItem(key);
+          updateDOM();
         }
       }
      } 
